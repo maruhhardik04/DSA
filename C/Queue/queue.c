9f6f3fa44503986002e59952caea828101f3917d
maruhardik04 <49114615+maruhhardik04@users.noreply.github.com>
2022-03-24 17:46:14 +0530

    Update queue.c

--- C/Queue/queue.c
+++ C/Queue/queue.c
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<limits.h>
 #include<stdbool.h>
+#include<string.h>
+#include<math.h>
 
 typedef struct Queue
 {
@@ -17,6 +19,7 @@ bool isFull(Queue* self);
 bool isEmpty(Queue* self);
 void insertAnElement(Queue* self);
 void removeAnElement(Queue* self);
+void display(Queue* self);
 
 Queue* creatQueue()
 {
@@ -25,10 +28,11 @@ Queue* creatQueue()
     scanf("%u",&Size); 
 
     Queue* queue=(Queue*)malloc(sizeof(Queue));
-    queue->Size=Size;
+    queue->Size=Size;                                                                                           
     queue->front=-1;
     queue->rear=-1;
     queue->queue=(int*)malloc(queue->Size*sizeof(int));
+    memset(queue->queue,0,queue->Size*sizeof(queue->queue[0]));
     return queue;
 }
 
@@ -63,7 +67,7 @@ void insertAnElement(Queue* self)
     self->queue[self->rear]=val;
     return ;
     }
-    printf("Queue is Full");
+    printf("Queue is Full\n");
     
 }
 
@@ -74,31 +78,128 @@ void removeAnElement(Queue* self)
         if(self->front==self->Size-1 && self->rear==self->Size-1)
         {
         	printf("\n%d is removed from the Queue",self->queue[self->front]);
+            self->queue[self->front]=0;
 			self->rear=-1;
         	self->front=-1;
         	
         }
         else
         {	
+            
         	printf("\n%d is removed from the Queue",self->queue[self->front]);
-        	self->front++;
+        	self->queue[self->front]=0;
+            self->front++;
 		}
         
        return ;
     }
-     printf("\nQueue is Empty");
+     printf("Queue is Empty\n");
 }
+
+void display(Queue* self){
+    char c;
+    FILE *fp=fopen("Queue.txt","w");
+    fprintf(fp,"\t\t\tQueue\n\n");
+    fprintf(fp,"\t\t\t|-----|\n");
+    if(isEmpty(self))
+    {
+        for (int i = 0; i < self->Size; i++)
+        {
+            fprintf(fp,"\t\t\t|     |\n");
+            fprintf(fp,"\t\t\t|-----|\n");
+        }
+        fprintf(fp,"\n\t\t\tQueue is Empty");
+    }
+    else{
+    
+        for (int i = 0; i < self->Size; i++)
+        {
+            if(i==self->rear)
+                fprintf(fp,"Rear->");
+            
+            if(self->queue[i]==0)
+                fprintf(fp,"\t\t\t|     |");
+            else
+                fprintf(fp,"\t\t\t|%3d  |",self->queue[i]);
+
+            if(i==self->front)
+                fprintf(fp,"<-Front");
+
+            fprintf(fp,"\n\t\t\t|-----|\n");
+        }
+        if(isFull(self))
+        {
+            fprintf(fp,"\n\t\t\tQueue is Full");
+        }
+    }
+    
+    fclose(fp);
+
+            fp=fopen("Queue.txt","r");
+            if(fp != NULL)
+            {
+                while ((c=fgetc(fp)) != EOF)
+                {
+                    printf("%c",c);    
+                }
+                fclose(fp);
+                return ;
+            }
+            printf("Cannot open file \n");
+
+}
+
 
 int main()
 {
+    unsigned ch;
     Queue *queue=creatQueue();
-    insertAnElement(queue);
-    insertAnElement(queue);
-    insertAnElement(queue);
-    removeAnElement(queue);
-    removeAnElement(queue);
-    removeAnElement(queue);
-    removeAnElement(queue);
-    removeAnElement(queue);
+    while(1)
+    {
+
+
+       printf("\n1.Insert An Element");
+       printf("\n2.Remove An Element");
+       printf("\n3.isFull"); 
+       printf("\n4.isEmpty");      
+       printf("\n5.Display"); 
+       printf("\n0.Exit"); 
+
+        printf("\nEnter operation:");
+        scanf("%u",&ch);
+        printf("\n\n");
+
+            if(ch==0)
+            {
+                break;
+            }
+
+            switch (ch)
+            {
+            case 1:
+                insertAnElement(queue);
+                break;
+            case 2:
+                removeAnElement(queue);
+                break;
+             case 3:
+                printf("%s",(isEmpty(queue)?"Yes":"No"));
+                break;    
+            case 4:
+                printf("%s",(isFull(queue)?"Yes":"No"));
+                break; 
+            case 5:
+                display(queue);
+                break;    
+
+            default:
+                printf("Invalid Operation");
+                break;
+            }
+
+
+    }
+    
+
 	return 0;
 }
